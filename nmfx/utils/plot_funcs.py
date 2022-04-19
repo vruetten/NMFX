fd3fa2f3383c3bfae5b17d752bd677c8b68b9ad2
vrutten <vms.rutten@gmail.com>
2022-04-19 19:10:42 -0400

    make into proper package

--- nmfx/utils/plot_funcs.py
+++ nmfx/utils/plot_funcs.py
@@ -5,21 +5,21 @@ def plot_loss(results_path, losses):
     pl.figure(figsize = (21, 4))
     title = 'loss'
     pl.title(title)
-    xax = np.arange(losses[0].shape[0])
-    pl.plot(xax, losses[0],'o-')
-    pl.xlabel('iteration #')
-    pl.tight_layout()
-    pl.savefig(results_path + title + '.png', transparent = True)
-
-    losses = losses.flatten()
-    pl.figure(figsize = (21, 4))
-    title = 'all loss'
-    pl.title(title)
-    xax = np.arange(len(losses))
+    xax = np.arange(losses.shape[0])
     pl.plot(xax, losses,'o-')
     pl.xlabel('iteration #')
     pl.tight_layout()
     pl.savefig(results_path + title + '.png', transparent = True)
+
+    # losses = losses.flatten()
+    # pl.figure(figsize = (21, 4))
+    # title = 'all loss'
+    # pl.title(title)
+    # xax = np.arange(len(losses))
+    # pl.plot(xax, losses,'o-')
+    # pl.xlabel('iteration #')
+    # pl.tight_layout()
+    # pl.savefig(results_path + title + '.png', transparent = True)
 
 
 
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+        name='nmfx',
+        version='0.1',
+        description='Stochastic Non-Negative Matrix Factorization',
+        url='https://github.com/vrutten/nmfx',
+        author='Virginia MS Rutten, Jan Funke',
+        author_email='ruttenv@janelia.hhmi.org, funkej@janelia.hhmi.org',
+        license='MIT',
+        packages=[
+            'nmfx'
+        ],
+        install_requires=[
+            'numpy',
+            'jax',
+            'tqdm'
+        ]
+)

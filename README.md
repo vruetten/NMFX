5e1fa7aa0dce0e4e5bf1db6d9adab735d2f5891b
vrutten <vms.rutten@gmail.com>
2023-03-08 10:33:34 -0500

    push readme to pypi

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Jaxed NMF
 
-NMF optimized through stochastic gradient descent
+NMF optimized through stochastic gradient descent with the option of adding l2 or l1 penalities on weight matrices. 
 
-## Usage
+NMF minimizes: 
+`L(W, H) = ||X-WH||_2 subject to W>0, H>0`
+
 
 
 
--- setup.py
+++ setup.py
@@ -1,23 +1,20 @@
 from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
-        name='nmfx',
-        version='0.1',
-        description='Stochastic Non-Negative Matrix Factorization',
-        url='https://github.com/vrutten/nmfx',
-        author='Virginia MS Rutten, Jan Funke',
-        author_email='ruttenv@janelia.hhmi.org, funkej@janelia.hhmi.org',
-        license='MIT',
-        packages=[
-            'nmfx'
-        ],
-        install_requires=[
-            'numpy',
-            'jax',
-            'tqdm'
-        ],
-        scripts=[
-            'scripts/nmfx',
-            'scripts/nmfx_hdf5'
-        ]
+    name="nmfx",
+    version="0.1",
+    description="Stochastic Non-Negative Matrix Factorization",
+    url="https://github.com/vrutten/nmfx",
+    author="Virginia MS Rutten, Jan Funke",
+    author_email="ruttenv@janelia.hhmi.org, funkej@janelia.hhmi.org",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=["nmfx"],
+    install_requires=["numpy", "jax", "tqdm"],
+    scripts=["scripts/nmfx", "scripts/nmfx_hdf5"],
 )
